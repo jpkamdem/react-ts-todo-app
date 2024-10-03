f1664c773f0e1131df284b75da30525a49f1fab6
jkam <jpkamdem@proton.me>
2024-10-04 00:37:05 +0200

    création de todo (chaque item) et todos (tableau de todo), réglage du typage via <type>

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
 
 export default function Todo() {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
 
   return (
-    <form>
-      <input
-        type="text"
-        onChange={(event) => setTodo(event.target.value)}
-        value={todo}
-      />
-      <button type="submit">Add</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(event) => setTodo(event.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((item) => (
+          <li key={item}> {item} </li>
+        ))}
+      </ul>
+    </>
   );
 }
