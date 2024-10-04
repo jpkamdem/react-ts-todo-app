0746d086d0d39725e82d9f89a6377b4615f4441e
jkam <jpkamdem@proton.me>
2024-10-04 18:12:38 +0200

    changement vraiment mineur

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
 import styles from "./header.module.css";
 export default function Header() {
-  return <div className={styles.header}>My Todo List</div>;
+  return <header className={styles.header}>My Todo List</header>;
 }
